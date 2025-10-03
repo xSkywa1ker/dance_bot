d1123d1bbd4916181c808259c380f4b4212fefd6
xSkywa1ker <92277526+xSkywa1ker@users.noreply.github.com>
2025-10-04 01:57:12 +0300

    Add package marker for dancestudio namespace

---
+++ dancestudio/__init__.py
@@ -0,0 +1,4 @@
+"""Top-level package for the Dance Studio project."""
+
+# This file enables imports that rely on the ``dancestudio`` namespace, such as
+# ``dancestudio.bot`` and ``dancestudio.backend``.
